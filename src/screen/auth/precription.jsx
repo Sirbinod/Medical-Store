a07ca0064e6eb32d99ad429b5a9db54b5b667cef
Sirbinod <00np10@gmail.com>
2021-09-30 21:04:26 +0545

    prescription upload

--- src/screen/auth/precription.jsx
+++ src/screen/auth/precription.jsx
@@ -1,17 +1,46 @@
 import React, { useState } from "react";
-import { Modal, Form, Button, Row, Col } from "react-bootstrap";
+import { Modal, Form, Button, Row, Col, Card} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import Dropzone from "react-dropzone";
+import { Link } from "react-router-dom";
+import { FaFileUpload } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 
 import { Field, reduxForm } from "redux-form";
-import { signupStart } from "../../store/action/profileAction";
+import { addPreception, signupStart } from "../../store/action/profileAction";
 
-import DropZoneField from "../../component/imageUpload/imageUpload";
+
 
 const Precription = (props, show) => {
   const { modelPrecription } = useSelector((state) => state.profile);
   const { handleSubmit, reset, pristine, submitting, imageFile, handleOnDrop } =
     props;
   const dispatch = useDispatch();
+  const [selectedFiles, setselectedFiles] = useState([]);
+
+  function handleAcceptedFiles(files) {
+    files.map((file) =>
+      Object.assign(file, {
+        preview: URL.createObjectURL(file),
+        formattedSize: formatBytes(file.size),
+      }),
+    );
+    setselectedFiles(files);
+  }
+
+  /**
+   * Formats the size
+   */
+  function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  }
+
   return (
     <div>
       <Modal
@@ -35,33 +64,69 @@ const Precription = (props, show) => {
             </div>
           </Col>
           <Col lg={7}>
-            <div className='login-form'>
-              <h3 className='login'>Prescription Upload Here</h3>
-              <form className='forms-sample'>
-                {/* <Field name='asdas'   /> */}
-                <Form.Group className='group-form'>
-                  <label htmlFor='email'>Image</label>
-                  {/* <Field
-                    name='image'
-                    component={DropZoneField}
-                    type='file'
-                    imageFile={imageFile}
-                    handleOnDrop={handleOnDrop}
-                  /> */}
-                </Form.Group>
+            <div className='close_div'>
+             
+              <ImCross className='close_icon' onClick={()=>dispatch(addPreception()) }/>
+             
+            </div>
 
-                <button
-                  type='submit'
-                  className='btn btn-primary mr-2'
-                  disabled={pristine || submitting}>
-                  Submit
-                </button>
-                <button
-                  className='btn btn-dark mx-2'
-                  onClick={() => dispatch(signupStart())}>
-                  Cancel
-                </button>
-              </form>
+            <div className='login-form'>
+              <div className='mb-5'>
+                <Form>
+                  <Dropzone
+                    onDrop={(acceptedFiles) => {
+                      handleAcceptedFiles(acceptedFiles);
+                    }}>
+                    {({ getRootProps, getInputProps }) => (
+                      <div className='dropzone'>
+                        <div
+                          className='dz-message needsclick'
+                          {...getRootProps()}>
+                          <input {...getInputProps()} />
+                          <div className='mb-3'>
+                            <FaFileUpload className='icon_upload' />
+                          </div>
+                          <h4>Drop files here or click to upload.</h4>
+                        </div>
+                      </div>
+                    )}
+                  </Dropzone>
+                  <div className='dropzone-previews mt-3' id='file-previews'>
+                    {selectedFiles.map((f, i) => {
+                      return (
+                        <Card
+                          className='mt-1 mb-0 shadow-none border dz-processing dz-image-preview dz-success dz-complete'
+                          key={i + "-file"}>
+                          <div className='p-2'>
+                            <Row className='align-items-center'>
+                              <Col className='col-auto'>
+                                <img
+                                  data-dz-thumbnail=''
+                                  height='200'
+                                  className='avatar-sm rounded bg-light'
+                                  alt={f.name}
+                                  src={f.preview}
+                                />
+                              </Col>
+                              <Col>
+                                <Link
+                                  to='#'
+                                  className='text-muted font-weight-bold'>
+                                  {f.name}
+                                </Link>
+                                <p className='mb-0'>
+                                  <strong>{f.formattedSize}</strong>
+                                </p>
+                              </Col>
+                            </Row>
+                          </div>
+                        </Card>
+                      );
+                    })}
+                  </div>
+                </Form>
+                <Button className='my-2'>Submit</Button>
+              </div>
             </div>
           </Col>
         </Row>
